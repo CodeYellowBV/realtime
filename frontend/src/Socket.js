92e310e640c96c9ad92418c2b41ede829c1400b1
Kees Kluskens <kees@webduck.nl>
2017-03-21 00:18:38 +0100

    Add heartbeat for websockets

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,6 +1,7 @@
 export default class Socket {
     instance = null;
     handlers = {};
+    pingInterval = null;
 
     constructor(handlers) {
         this.handlers = handlers;
@@ -12,16 +13,26 @@ export default class Socket {
 
         this.instance.onopen = () => {
             this.handlers.onOpen();
+            this.pingInterval = setInterval(() => {
+                this.instance.send('ping');
+            }, 30000);
         };
 
         this.instance.onclose = () => {
             this.handlers.onClose();
+            if (this.pingInterval) {
+                clearInterval(this.pingInterval);
+                this.pingInterval = null;
+            }
             setTimeout(() => {
                 this.initialize();
             }, 2000);
         };
 
         this.instance.onmessage = (evt) => {
+            if (evt === 'pong') {
+                return;
+            }
             const msg = JSON.parse(evt.data);
             console.log('[received]', msg.type, msg.data);
             this.handlers.onMessage(msg.type, msg.data);
