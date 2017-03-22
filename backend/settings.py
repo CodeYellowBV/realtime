f950319212021bd54a1c8a3c7d992dc4f979c59e
JasperStam <jasper@codeyellow.nl>
2017-03-22 21:32:07 +0100

    Fix oAuth in backend

--- backend/settings.py
+++ backend/settings.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+
 class Settings(object):
-	DEBUG = os.environ.get('CY_DEBUG')
-	CSRF_ENABLED = True
-	SECRET_KEY = os.environ.get('CY_SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://' + os.environ.get('CY_DB_USER') + '@/' + os.environ.get('CY_DB_NAME')
+    DEBUG = os.environ.get('CY_DEBUG')
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('CY_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://' + os.environ.get('CY_DB_USER') + '@/' + os.environ.get('CY_DB_NAME')
--- backend/src/controller.py
+++ backend/src/controller.py
@@ -1,5 +1,6 @@
 import json
-# from app import app
+import os
+import requests
 from models import Entry, Project
 
 
@@ -9,12 +10,15 @@ class Controller():
     # Split the type into a method and a class
     def handle(self, db, message):
         if message == 'ping':
+            # todo keepalive logic
             return 'pong'
 
         body = json.loads(message)
 
+        if body['type'] == 'authenticate':
+            return self.auth(db, body)
         # TODO just split on first uppercase
-        if body['type'].startswith('save'):
+        elif body['type'].startswith('save'):
             method = 'save'
         elif body['type'].startswith('get'):
             method = 'get'
@@ -43,3 +47,39 @@ class Controller():
 
         result = m.dump()
         return json.dumps(result)
+
+    def auth(self, db, body):
+        print('auth')
+        data = {
+            'client_id': os.environ.get('CY_APP_PHABRICATOR_CLIENT_ID'),
+            'client_secret': os.environ.get('CY_APP_PHABRICATOR_CLIENT_SECRET'),
+            'redirect_uri': os.environ.get('CY_APP_REDIRECT_URI'),
+            'code': body['data']['code'],
+            'grant_type': 'authorization_code',
+        }
+
+        r1 = requests.post(os.environ.get('CY_APP_PHABRICATOR_URL') + '/oauthserver/token/', params=data)
+
+        if r1.status_code != 200:
+            return json.dumps({
+                'type': 'authenticate',
+                'code': 'fail',
+                'message': r1.json()['error_description']
+            })
+
+        token = r1.json()['access_token']
+
+        r2 = requests.get(os.environ.get('CY_APP_PHABRICATOR_URL') + '/api/user.whoami', params={'access_token': token})
+
+        res = r2.json()['result']
+
+        return json.dumps({
+            'type': 'authenticate',
+            'data': {
+                'id': 'TODO',
+                'username': res['userName'],
+                'email': res['primaryEmail'],
+                'avatar_url': res['image'],
+                'display_name': res['realName'],
+            }
+        })
