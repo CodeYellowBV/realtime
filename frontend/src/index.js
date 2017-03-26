c812ec4a225d82f3cdd49f7b69f5e3d5c0a90d0f
Kees Kluskens <kees@webduck.nl>
2017-03-26 16:10:06 +0200

    Remove mobx-remotedev

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import remotedev from 'mobx-remotedev';
 import 'normalize.css';
 import App from './container/App';
 import ViewStore from './store/View';
 import startRouter from './router';
 
 const viewStore = new ViewStore();
-
-// This enables easy MobX debugging with a Chrome Extension.
-// The code will be excluded on production automatically.
-// For info on how to use: https://github.com/zalmoxisus/mobx-remotedev
-remotedev(viewStore);
 
 const router = startRouter(viewStore);
 viewStore.router = router;
