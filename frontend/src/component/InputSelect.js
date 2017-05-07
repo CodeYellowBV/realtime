466c4e64628c7239d7c4821af3fd912f289e895f
Rutger Schimmel <rma.schimmel@gmail.com>
2017-05-07 19:30:57 +0200

    Style project input selector

--- frontend/src/component/InputSelect.js
+++ frontend/src/component/InputSelect.js
@@ -2,6 +2,47 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { observer, PropTypes as MobxTypes } from 'mobx-react';
 import Select from 'react-select';
+import styled from 'styled-components';
+
+const StyledSelect = styled(Select)`
+    .Select-control {
+        height: 48px;
+        border: 0;
+        border-radius: 8px;
+        cursor: pointer;
+    }
+
+    .Select-input {
+        height: 48px;
+    }
+
+    .Select-input > input {
+        line-height: 28px;
+    }
+
+    .Select-placeholder,
+    .Select-control .Select-value {
+        line-height: 48px !important;
+    }
+
+    .Select-value-label {
+        color: #000 !important;
+    }
+
+    .Select-menu-outer {
+        left: 0;
+        right: 0;
+        border: 0;
+        border-top: 1px solid #ddd;
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+
+    .Select-option:last-child {
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+`;
 
 @observer
 export default class InputSelect extends Component {
@@ -31,7 +72,7 @@ export default class InputSelect extends Component {
 
     render() {
         return (
-            <Select
+            <StyledSelect
                 value={this.props.value}
                 options={this.props.options}
                 onChange={this.handleChange}
