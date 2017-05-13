c8cded91bff06fd4ab4fb319b656523486bf0650
Kees Kluskens <kees@webduck.nl>
2017-05-13 12:30:29 +0200

    add reusable desktop query

--- frontend/src/component/TimeEntryForm.js
+++ frontend/src/component/TimeEntryForm.js
@@ -1,23 +1,24 @@
 import Form from './Form';
 import FormField from './FormField';
 import styled from 'styled-components';
+import { desktop } from '../styles';
 
 export const TimeEntryForm = styled(Form)`
     display: flex;
     margin-bottom: 32px;
     align-items: flex-end;
 
-    @media (max-width: 400px) {
+    ${desktop`
         flex-direction: column;
         align-items: stretch;
-    }
+    `}
 `;
 
 export const TimeEntryFormField = styled(FormField)`
     flex: ${props => props.size || 0};
     margin: 0 8px 8px 8px;
 
-    @media (max-width: 400px) {
+    ${desktop`
         ${props => props.center && `align-self: center`}
-    }
+    `}
 `;
--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -1,7 +1,17 @@
-import { injectGlobal } from 'styled-components';
+import { injectGlobal, css } from 'styled-components';
 import 'react-select/dist/react-select.css';
 
 export const COLOR_TINT = '#eecb3d';
+
+const DESKTOP_QUERY = '(max-width: 768px)';
+
+export function desktop(...args) {
+    return css`
+        @media ${DESKTOP_QUERY} {
+            ${css(...args)}
+        }
+    `;
+}
 
 export default injectGlobal`
     html {
