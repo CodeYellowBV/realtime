cf20ef01806372259088037947f45cba24e6e9c9
Kees Kluskens <kees@webduck.nl>
2017-05-16 18:20:19 +0200

    Add renaming project
    Fixes #43

--- frontend/src/container/ProjectOverviewItem.js
+++ frontend/src/container/ProjectOverviewItem.js
@@ -1,20 +1,41 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 import {
     EntryItem,
     EntryItemProject,
     EntryItemActions,
 } from '../component/EntryList';
 import Icon from '../component/Icon';
-import Link from '../component/Link';
 import IconDelete from 'image/icon-delete.svg';
+import IconEdit from 'image/icon-edit.svg';
 import { Project } from '../store/Project';
+import ProjectName from '../component/Project/Name';
 
 @observer
 export default class ProjectOverviewItem extends Component {
     static propTypes = {
         project: PropTypes.instanceOf(Project).isRequired,
+    };
+
+    @observable editing = false;
+
+    stopEditing = () => {
+        this.editing = false;
+        this.props.project.save();
+    };
+
+    startEditing = () => {
+        this.editing = true;
+    };
+
+    toggleEditing = () => {
+        if (this.editing) {
+            this.stopEditing();
+        } else {
+            this.startEditing();
+        }
     };
 
     handleDelete = () => {
@@ -31,11 +52,14 @@ export default class ProjectOverviewItem extends Component {
         return (
             <EntryItem>
                 <EntryItemProject>
-                    <Link to={`/project/entries/${project.id}`}>
-                        {project.name}
-                    </Link>
+                    <ProjectName
+                        project={project}
+                        editing={this.editing}
+                        onClose={this.stopEditing}
+                    />
                 </EntryItemProject>
                 <EntryItemActions>
+                    <Icon onClick={this.toggleEditing} icon={IconEdit} />
                     <Icon onClick={this.handleDelete} icon={IconDelete} />
                 </EntryItemActions>
             </EntryItem>
