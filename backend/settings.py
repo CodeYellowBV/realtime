562c91dfd75fa3ae50cf57e468928a52cd4f1eb1
stefanmajoor <stefan@codeyellow.nl>
2019-02-19 16:17:43 +0100

    Add a CY_DB_HOST setting

--- backend/settings.py
+++ backend/settings.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(os.environ.get('CY_ENV_FILE', find_dotenv()))
 
-
 class Settings(object):
     DEBUG = os.environ.get('CY_DEBUG')
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('CY_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://' + os.environ.get('CY_DB_USER') + '@/' + os.environ.get('CY_DB_NAME')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://' + os.environ.get('CY_DB_USER') + '@' +\
+                              os.environ.get('CY_DB_HOST', '') + '/' + os.environ.get('CY_DB_NAME')
+
